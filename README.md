0c567086e50d680ae2a0559003c357e4d90a9ee9
Lucas Mendes <lucasmendesifpr@gmail.com>
2020-05-15 11:22:41 -0300

    inicializando Projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# App para estudo de Constraint Layout
---
+++ app/src/main/java/com/lucas/viagensconstraint/ui/activity/ListaPacotesActivity.kt
@@ -0,0 +1,13 @@
+package com.lucas.viagensconstraint.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucas.viagensconstraint.R
+
+class ListaPacotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
